e3a902c8e54a473bd64e947aae5a45f6e760de95
letsgo-rookie <zshw007@163.com>
2020-10-14 11:23:31 +0800

    mybatis-plus demo，mainly include CRUD operations.

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,11 +1,38 @@
 package com.example.demo.entity;
 
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.Version;
+
 import lombok.Data;
 
 @Data
 public class User {
+	
+//	@TableId(type = IdType.AUTO)
 	private Long id;
+	
 	private String name;
 	private Integer age;
 	private String email;
+	
+//	@TableField(fill = FieldFill.INSERT)
+//	private Date createTime;
+//	
+//	//@TableField(fill = FieldFill.UPDATE)
+//	@TableField(fill = FieldFill.INSERT_UPDATE)
+//	private Date updateTime;
+	
+	@Version
+	@TableField(fill = FieldFill.INSERT)
+	private Integer version;
+	
+	@TableLogic
+	@TableField(fill = FieldFill.INSERT)
+	private Integer deleted;
 }
